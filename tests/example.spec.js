5f57de277851e755819fdb40041950d6a06a7fc1
sruthi <sruthikg2010@gmail.com>
2026-07-26 22:34:33 +0100

    second commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,6 +7,8 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
+//test statement
+//new line
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
